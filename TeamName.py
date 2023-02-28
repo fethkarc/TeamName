63b25ee59c79ab5f1cdd9042c304f1ef6faa18c9
Fethi Can Karacan <114345653+fethkarc@users.noreply.github.com>
2023-02-28 17:10:30 +0300

    Add files via upload

---
+++ TeamName.py
@@ -0,0 +1,4 @@
+print("Welcome to the Team Name Generator.")
+o = input("What is the nearest object to you?\n")
+f = input("What is your biggest fear?\n")
+print("Your team name could be " + o + " " + f)
